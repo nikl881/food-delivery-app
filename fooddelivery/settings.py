8d0dd48ce0a15228ef98b437bcf9bc49ba8cfe78
Niels <deklerkn@xs4all.nl>
2019-05-22 14:33:45 +0000

    if else for db for travis

--- fooddelivery/settings.py
+++ fooddelivery/settings.py
@@ -1,6 +1,6 @@
 
 import os
-import env
+# import env
 import dj_database_url
 
 
@@ -70,18 +70,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'fooddelivery.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+# DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    print("Database URL not found. Using SQLite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
