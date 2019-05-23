17d1dd29e3c8a81fc7faa71f5416f68d4d08d652
Niels <deklerkn@xs4all.nl>
2019-05-23 05:41:01 +0000

    added allowed host for Heroku

--- fooddelivery/settings.py
+++ fooddelivery/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), '127.0.0.1']
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), '127.0.0.1', 'akitos-sushi-express.herokuapp.com']
 
 
 # Application definition
