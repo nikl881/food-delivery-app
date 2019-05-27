d92b60ba8b72e1251c37ba23a179941528e97ebf
Niels <deklerkn@xs4all.nl>
2019-05-27 07:07:31 +0000

    working with comments

--- home/views.py
+++ home/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     """A view that displays the index page"""
-    return render(request, "index.html")
+    return render(request, "home/index.html")
