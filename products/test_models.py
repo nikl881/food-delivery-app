aa5f8c5ebe6b623aa38ec5c29ec4e989382f9fa1
Niels <deklerkn@xs4all.nl>
2019-05-30 08:44:03 +0000

    created form validation test for required fields on checkout app

--- products/test_models.py
+++ products/test_models.py
@@ -5,13 +5,13 @@ from .products import Item
 class TestItemModel(TestCase):
 
     def test_done_defaults_to_False(self):
-        item = Item(name="Create a Test")
+        item = Item(name="Create Test")
         item.save()
-        self.assertEqual(item.name, "Create a Test")
+        self.assertEqual(item.name, "Create Test")
         self.assertFalse(item.done)
     
     def test_can_create_an_item_with_a_name_and_status(self):
-        item = Item(name="Create a Test", done=True)
+        item = Item(name="Create Test", done=True)
         item.save()
-        self.assertEqual(item.name, "Create a Test")
+        self.assertEqual(item.name, "Create  Test")
         self.assertTrue(item.done)
