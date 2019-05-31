5d800c1742ab65cfb3cb0e853cb0f2fdb83e173a
Niels <deklerkn@xs4all.nl>
2019-05-31 07:26:58 +0000

    added models test to products app

--- products/models.py
+++ products/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 # Will create models
+
 class Product(models.Model):
-    name = models.CharField(max_length=254, default='')
+    name = models.CharField(max_length=254, blank=False)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
--- products/test_models.py
+++ products/test_models.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 from .models import Product
 
+# testing models 
+
+class TestModelProduct(TestCase):
+
+    def test_done_defaults_to_False(self):
+        Product = Product(name="Create a Test")
+        Product.save()
+        self.assertEqual(Product.name, "Create a Test")
+        self.assertFalse(Product.done)
+    
+    def test_can_create_an_item_with_a_name_and_status(self):
+        Product = Product(name="Create a Test", done=True)
+        Product.save()
+        self.assertEqual(Product.name, "Create a Test")
+        self.assertTrue(Product.done)
