24393e6c9f9fc04eb3dfa40960c148089a068292
Niels <deklerkn@xs4all.nl>
2019-06-03 06:41:24 +0000

    removed unused patterns from urls.py

--- fooddelivery/urls.py
+++ fooddelivery/urls.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 from accounts import urls as urls_accounts
 from products import urls as urls_products
 from cart import urls as urls_cart
-from search import urls as urls_search
 from checkout import urls as urls_checkout
 from products.views import all_products
 from home.views import index
@@ -21,6 +20,5 @@ urlpatterns = [
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
     url(r'^checkout/', include(urls_checkout)),
-    url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
